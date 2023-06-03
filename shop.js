62b97d51ec402640476c1e86a9e5ada3d7d70ba9
kkocx-Chen <cspss9928@gmail.com>
2023-06-03 23:42:48 +0800

    移除了 shop 下拉選單 並且加大字體

--- shop.js
+++ shop.js
@@ -1,22 +1,28 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // 獲取下拉選單元素
-    var dropdownMenu = document.getElementById("dropdownMenu");
+// 獲取下拉選單元素
+const dropdownMenu = document.getElementById('dropdownMenuButton1');
+// 獲取價格元素
+const priceElement = document.getElementById('price');
+// 獲取立即購買按鈕元素
+const purchaseBtn = document.getElementById('purchaseBtn');
 
-    // 確認 dropdownMenu 存在後再執行後續操作
-    if (dropdownMenu) {
-        // 獲取下拉選單中所有選項的 <a> 元素
-        var dropdownItems = dropdownMenu.querySelectorAll("a.dropdown-item");
-
-        // 確認 dropdownItems 存在後再執行後續操作
-        if (dropdownItems.length > 0) {
-            // 為每個選項添加點擊事件處理程序
-            dropdownItems.forEach(function (item) {
-                item.addEventListener("click", function (event) {
-                    event.preventDefault(); // 阻止點擊事件的預設行為
-                    var selectedValue = item.getAttribute("data-value"); // 獲取選項的值
-                    console.log(selectedValue); // 印出選擇的值，可根據需要進行後續操作
-                });
-            });
-        }
+// 監聽下拉選單選擇事件
+dropdownMenu.addEventListener('click', function(event) {
+    // 阻止默認事件
+    event.preventDefault();
+    // 獲取選擇的值
+    const selectedValue = event.target.getAttribute('data-value');
+    // 根據選擇的值更新價格
+    if (selectedValue === '1') {
+        priceElement.textContent = '價格: $99';
+    } else if (selectedValue === '2') {
+        priceElement.textContent = '價格: $199';
+    } else if (selectedValue === '3') {
+        priceElement.textContent = '價格: $299';
     }
+});
+
+// 監聽立即購買按鈕點擊事件
+purchaseBtn.addEventListener('click', function() {
+    // 在此處執行立即購買的相應操作
+    console.log('立即購買');
 });
