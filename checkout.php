31fee3b174ec64b13fb617a8f51bc4b48d2060d3
kkocx-Chen <cspss9928@gmail.com>
2023-06-06 09:25:05 +0800

    更新php和後端做查詢

--- checkout.php
+++ checkout.php
@@ -1,17 +1,30 @@
 <?php
-// checkout.php
+// 建立與資料庫的連線
+$servername = "localhost"; // 資料庫伺服器名稱
+$username = "your_username"; // 使用者名稱
+$password = "your_password"; // 使用者密碼
+$dbname = "your_database"; // 資料庫名稱
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // 取得產品 ID
-    $productId = $_POST['product_id'];
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-    // 在這裡處理購買邏輯，包括生成序號和儲存到資料庫中
-    $serialNumber = generateSerialNumber(); // 假設有一個函式生成序號
-
-    // 將序號儲存到資料庫或其他儲存方式中
-
-    // 重導向到包含序號的頁面
-    header('Location: serial_number.php?serial=' . urlencode($serialNumber));
-    exit();
+// 檢查連線是否成功
+if ($conn->connect_error) {
+    die("連線失敗：" . $conn->connect_error);
 }
+
+// 從資料庫中取得資料
+$sql = "SELECT * FROM your_table"; // 資料表名稱
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // 迭代輸出每一筆資料
+    while ($row = $result->fetch_assoc()) {
+        echo "序號：" . $row["serial_number"] . "<br>";
+    }
+} else {
+    echo "沒有資料";
+}
+
+// 關閉資料庫連線
+$conn->close();
 ?>
