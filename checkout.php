ba3e5782e9bae191829025d7151cf3005c0c093e
kkocx-Chen <cspss9928@gmail.com>
2023-06-06 13:29:50 +0800

    測試iframe

--- checkout.php
+++ checkout.php
@@ -5,6 +5,18 @@
     <title>顯示資料</title>
     <!-- 引入 Bootstrap 的 CSS 檔案 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <style>
+        .container {
+            height: 100vh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .card-columns {
+            margin: 0 auto;
+        }
+    </style>
 </head>
 <body>
 
@@ -26,17 +38,15 @@ $sql = "SELECT * FROM Win11"; // 資料表名稱
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    echo '<div class="container mt-4">'; // 加入 mt-4 類別調整上邊距
-    echo '<div class="row justify-content-center">'; // 使用 justify-content-center 將卡片容器置中
+    echo '<div class="container mt-4">';
+    echo '<div class="card-columns">';
 
-    // 迭代輸出每一筆資料
+    // 迭代输出每一笔数据
     while ($row = $result->fetch_assoc()) {
-        echo '<div class="col-md-4">';
-        echo '<div class="card mb-3 text-center">'; // 使用 text-center 將卡片內容置中
+        echo '<div class="card mb-3 text-center">';
         echo '<div class="card-body">';
         echo '<h5 class="card-title">序號</h5>';
         echo '<p class="card-text">' . $row["serial_number"] . '</p>';
-        echo '</div>';
         echo '</div>';
         echo '</div>';
     }
@@ -46,7 +56,6 @@ if ($result->num_rows > 0) {
 } else {
     echo '沒有資料';
 }
-
 
 // 關閉資料庫連線
 $conn->close();
