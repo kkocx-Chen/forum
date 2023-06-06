b9d427c8040bbf947b8b9539063e7462323755fa
kkocx-Chen <cspss9928@gmail.com>
2023-06-06 10:14:59 +0800

    新增序號查詢頁面

--- serial_number.php
+++ serial_number.php
@@ -1,10 +1,10 @@
 <?php
-// 建立與資料庫的連線
 $servername = "terminal.kkocx.com"; // 資料庫伺服器名稱
 $username = "User01"; // 使用者名稱
 $password = "Test01"; // 使用者密碼
 $dbname = "user01"; // 資料庫名稱
 
+// 建立與資料庫的連線
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // 檢查連線是否成功
@@ -12,11 +12,13 @@ if ($conn->connect_error) {
     die("連線失敗：" . $conn->connect_error);
 }
 
-// 更新 serial_number 的值
-$newSerialNumber = "TFP9Y-VCY3P-VVH3T-8XXCC-MF4YK"; // 新的序號值
-$sql = "UPDATE Win11 SET serial_number = '$newSerialNumber'";
+$serialNumber = "TFP9Y-VCY3P-VVH3T-8XXCC-MF4YK"; // 要插入的序號值
+
+// 構建 INSERT 語句並執行
+$sql = "INSERT INTO Win11 (serial_number) VALUES ('$serialNumber')";
+
 if ($conn->query($sql) === TRUE) {
-    echo "serial_number 已成功更新";
+    echo "序號已成功插入";
 } else {
     echo "發生錯誤：" . $conn->error;
 }
