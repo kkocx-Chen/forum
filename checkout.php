230e62f0607a852aaa33f2042d306af2d8d88d7c
kkocx-Chen <cspss9928@gmail.com>
2023-06-06 10:57:05 +0800

    新增序號查詢頁面

--- checkout.php
+++ checkout.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>顯示資料</title>
+    <!-- 引入 Bootstrap 的 CSS 檔案 -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0/css/bootstrap.min.css">
+</head>
+<body>
+
 <?php
 // 建立與資料庫的連線
 $servername = "terminal.kkocx.com"; // 資料庫伺服器名稱
@@ -17,22 +27,31 @@ $sql = "SELECT * FROM Win11"; // 資料表名稱
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    // 建立一個 HTML 表格來顯示資料
-    echo '<table class="table table-striped">';
-    echo '<thead><tr><th>序號</th></tr></thead>';
-    echo '<tbody>';
+    echo '<div class="container">';
+    echo '<div class="row">';
 
     // 迭代輸出每一筆資料
     while ($row = $result->fetch_assoc()) {
-        echo '<tr><td>' . $row["serial_number"] . '</td></tr>';
+        echo '<div class="col-md-4">';
+        echo '<div class="card mb-3">';
+        echo '<div class="card-body">';
+        echo '<h5 class="card-title">序號</h5>';
+        echo '<p class="card-text">' . $row["serial_number"] . '</p>';
+        echo '</div>';
+        echo '</div>';
+        echo '</div>';
     }
 
-    echo '</tbody></table>';
+    echo '</div>';
+    echo '</div>';
 } else {
     echo '沒有資料';
 }
 
 
+
 // 關閉資料庫連線
 $conn->close();
 ?>
+</body>
+</html>
