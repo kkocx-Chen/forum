96bae426202ccf26a109bb902da196f952d9b836
kkocx-Chen <cspss9928@gmail.com>
2023-06-06 11:01:07 +0800

    新增序號查詢頁面並加入了bootstrap5

--- checkout.php
+++ checkout.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>顯示資料</title>
     <!-- 引入 Bootstrap 的 CSS 檔案 -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 <body>
 
