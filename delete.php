c37efc143b7a442dbc363c93f73943ca2b978c71
kkocx-Chen <cspss9928@gmail.com>
2023-06-13 09:56:18 +0800

    新增刪除數據庫

--- delete.php
+++ delete.php
@@ -2,44 +2,38 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>删除数据</title>
+    <title>刪除數據</title>
     <!-- 引入 Bootstrap 的 CSS 文件 -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body>
 
 <div class="container mt-4">
-    <h1>删除数据</h1>
-
-    <form method="post">
-        <button type="submit" name="delete" class="btn btn-danger">删除所有数据</button>
-    </form>
+    <h1>刪除數據</h1>
 
     <?php
-    // 建立与数据库的连接
-    $servername = "terminal.kkocx.com"; // 数据库服务器名称
-    $username = "User01"; // 用户名
-    $password = "Test01"; // 密码
-    $dbname = "user01"; // 数据库名称
+    // 建立與資料庫的連線
+    $servername = "terminal.kkocx.com"; // 資料庫伺服器名稱
+    $username = "User01"; // 使用者名稱
+    $password = "Test01"; // 使用者密碼
+    $dbname = "user01"; // 資料庫名稱
 
     $conn = new mysqli($servername, $username, $password, $dbname);
 
-    // 检查连接是否成功
+    // 檢查連線是否成功
     if ($conn->connect_error) {
-        die("连接失败：" . $conn->connect_error);
+        die("連線失敗：" . $conn->connect_error);
     }
 
-    // 处理删除操作
-    if (isset($_POST['delete'])) {
-        $sql = "DELETE FROM Win11";
-        if ($conn->query($sql) === TRUE) {
-            echo '<div class="alert alert-success">数据删除成功！</div>';
-        } else {
-            echo '<div class="alert alert-danger">数据删除失败：' . $conn->error . '</div>';
-        }
+    // 刪除所有數據
+    $sql = "DELETE FROM Win11";
+    if ($conn->query($sql) === TRUE) {
+        echo '<div class="alert alert-success">已刪除所有數據！</div>';
+    } else {
+        echo '<div class="alert alert-danger">數據刪除失敗：' . $conn->error . '</div>';
     }
 
-    // 关闭数据库连接
+    // 關閉資料庫連線
     $conn->close();
     ?>
 </div>
